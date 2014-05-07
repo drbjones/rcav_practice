a2f79f96737bcd7480dc1ab51f6a622d6d673994
drbjones <b-jones@kellogg.northwestern.edu>
2014-05-07 15:46:38 -0500

    Part 1 solution

---
+++ app/controllers/calculations_controller.rb
@@ -0,0 +1,21 @@
+class CalculationsController < ApplicationController
+  def home
+  end
+
+  def sqr
+    @the_number = params[:number].to_f
+    @answer = @the_number**2
+  end
+
+  def sqrt
+    @the_number = params[:number].to_f
+    @answer = Math.sqrt(@the_number)
+  end
+
+  def pmt
+    rate = params[:interest_rate].to_f/100
+    nper = params[:number_of_payments].to_f
+    pv = params[:present_value].to_f
+    @payment = ((rate*pv)*((1+rate)**nper))/(((1+rate)**nper)-1)
+  end
+end
